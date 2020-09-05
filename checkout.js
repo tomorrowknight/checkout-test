ec036c7fc925f56817f5814aaa72e1ac1585f912
Alvin Arulselvan <alvin@garuda.io>
2020-09-05 20:00:30 +0800

    Testing failure details again

--- checkout.js
+++ checkout.js
@@ -51,7 +51,10 @@ function getPaymentDetails(){
     let cko_session_id_param = urlParams.get('cko-session-id');
     const cko_session_id = cko_session_id_param;
     const auth_key = "sk_test_0b9b5db6-f223-49d0-b68f-f6643dd4f808";
-    
+    const payment_detail_div = document.getElementById("payment-detail");
+    if(payment_detail_div){
+      payment_detail_div.remove();
+    }
     $.ajax({
       type: "GET",
       url: "https://api.sandbox.checkout.com/payments/" + cko_session_id,
